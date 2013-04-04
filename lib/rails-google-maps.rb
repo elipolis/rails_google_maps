c4b52d3c4d2d0253580481ce0197b9105af5ce2d
Yuri Ratanov <organium@gmail.com>
2013-04-04 15:24:25 +0800

    wrong filenaming fixed

---
+++ lib/rails-google-maps.rb
@@ -0,0 +1,2 @@
+require "rails-google-maps/version"
+require "rails-google-maps/rails" if defined?(Rails)
--- lib/rails_google_maps.rb
+++
@@ -1,2 +0,0 @@
-require "rails_google_maps/version"
-require "rails_google_maps/rails" if defined?(Rails)
