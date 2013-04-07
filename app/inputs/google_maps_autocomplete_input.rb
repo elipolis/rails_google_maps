9330efddf55629f06c4e32f3bbb511a963b5fd30
Yuri Ratanov <organium@gmail.com>
2013-04-07 12:34:13 +0800

    SimpleForm input add apply parameter in order to have ability to not apply googlemaps on input creation. Global autocomplete oject renamed to gmapsAutocomplete.

--- app/inputs/google_maps_autocomplete_input.rb
+++ app/inputs/google_maps_autocomplete_input.rb
@@ -2,13 +2,14 @@ class GoogleMapsAutocompleteInput < SimpleForm::Inputs::StringInput
   include ActionView::Helpers::TagHelper
   def input
     gmap_id = input_html_options.delete(:gmap_id)
+    apply = input_html_options.delete(:apply)
     input = @builder.text_field(attribute_name, input_html_options)
-    (input + content_tag('script', script_text(gmap_id, input).html_safe)).html_safe
+    (input + content_tag('script', script_text(gmap_id, input, apply).html_safe)).html_safe
   end
 
   private
 
-  def script_text(gmap_id, tag)
+  def script_text(gmap_id, tag, apply = true)
     "$(document).ready(function(){
       if(typeof google == 'undefined')
         return;
@@ -20,9 +21,11 @@ class GoogleMapsAutocompleteInput < SimpleForm::Inputs::StringInput
       input = $('#{tag}');
       var id = input.attr('id');
       var location = input.val();
-      window.googleAutocomplete = new GmapAutocomplete('#' + id, gmap);
-      gmap.apply();
-      googleAutocomplete.apply();
+      window.gmapAutocomplete = new GmapAutocomplete('#' + id, gmap);
+      if(#{apply}){
+        gmap.apply();
+        gmapAutocomplete.apply();
+      }
       if(location.length > 0){
         gmap.setMarker('address', location);
       }
